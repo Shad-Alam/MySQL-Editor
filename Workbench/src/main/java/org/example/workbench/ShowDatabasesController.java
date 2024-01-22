c458d17ae9fea80cb33fa5a9c5641f5114fb3662
Shad-Alam <shad.19bd@gmail.com>
2024-01-22 22:03:52 +0600

    adding ShowDatabasesController

---
+++ Workbench/src/main/java/org/example/workbench/ShowDatabasesController.java
@@ -0,0 +1,23 @@
+package org.example.workbench;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TreeTableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ShowDatabasesController implements Initializable {
+    public TreeTableView tble_view;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void btn_create_new_schema(ActionEvent actionEvent) {
+    }
+
+    public void btn_logout(ActionEvent actionEvent) {
+    }
+}
--- Workbench/src/main/java/org/example/workbench/UserLogInController.java
+++ Workbench/src/main/java/org/example/workbench/UserLogInController.java
@@ -1,18 +1,19 @@
 package org.example.workbench;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ResourceBundle;
-
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class UserLogInController implements Initializable {
     public Button btn_id_connect;
@@ -33,7 +34,16 @@ public class UserLogInController implements Initializable {
             Class.forName("com.mysql.jdbc.Driver");
             connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/?user=root&password=shad");
             statement = connection.createStatement();
-            int result = statement.executeUpdate("create database jee");
+            String sql = "SHOW DATABASES";
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            int result = 0;
+            while(resultSet.next()){
+                String dbname = resultSet.getString("Database");
+                result++;
+                System.out.println(dbname);
+            }
+
             if (result>0){
                 connection.close();
                 return true;
@@ -47,10 +57,17 @@ public class UserLogInController implements Initializable {
         }
     }
 
-    public void btn_connect(ActionEvent actionEvent) {
+    public void btn_connect(ActionEvent actionEvent) throws IOException {
         String password = tf_password.getText();
         // check password match or not
         if(checkPassword(password)){
+            Parent parent =
+                    FXMLLoader.load(getClass().getResource("showDatabases.fxml"));
+            Scene scene = new Scene(parent);
+            Stage stage =
+                    (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
             System.out.println("Connected Successfully");
             // give a success message
         }else{
