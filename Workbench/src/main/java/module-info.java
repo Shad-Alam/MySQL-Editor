158a295172ebba5967abc7c9963b4d0f40ca89d8
Shad-Alam <shad.19bd@gmail.com>
2024-01-22 21:42:15 +0600

    database connenction testing successfully

--- Workbench/src/main/java/module-info.java
+++ Workbench/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.workbench {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.workbench to javafx.fxml;
--- Workbench/src/main/java/org/example/workbench/UserLogInController.java
+++ Workbench/src/main/java/org/example/workbench/UserLogInController.java
@@ -6,7 +6,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 
 import java.net.URL;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class UserLogInController implements Initializable {
     public Button btn_id_connect;
@@ -18,7 +24,27 @@ public class UserLogInController implements Initializable {
     }
 
     private boolean checkPassword(String password){
-        return password.equals("shad");
+        if(!password.equals("shad")){
+            return false;
+        }
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/?user=root&password=shad");
+            statement = connection.createStatement();
+            int result = statement.executeUpdate("create database jee");
+            if (result>0){
+                connection.close();
+                return true;
+            }else{
+                return false;
+            }
+        } catch (ClassNotFoundException e) {
+            return false;
+        } catch (SQLException e) {
+           return false;
+        }
     }
 
     public void btn_connect(ActionEvent actionEvent) {
