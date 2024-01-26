652ef830f6c13e6cb12051a16c4a46b9a8a6faa7
Shad-Alam <shad.19bd@gmail.com>
2024-01-26 14:46:18 +0600

    update changes

--- Workbench/src/main/java/org/example/workbench/App.java
+++ Workbench/src/main/java/org/example/workbench/App.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("showDatabases.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("userLogIn.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Mysql Workbench");
         stage.setScene(scene);
         stage.show();
